0a6d5d484e2c88f44f4f38300156c82149dfc1a9
Joshua Schulz-Andres <126828137+MrJSA@users.noreply.github.com>
2024-03-02 19:46:06 +0100

    Update AddBadWord.cs

--- BadWordFilter/AddBadWord.cs
+++ BadWordFilter/AddBadWord.cs
@@ -5,20 +5,27 @@ public class CPHInline
 {
     public bool Execute()
     {
+        // Get raw input of the Bad Word
         string rawInput = args["rawInput"].ToString();
-        CPH.SetArgument("newBannedWord", rawInput);
+        // Get User Name
         string user = args["userName"].ToString();
+        // Get .txt file Path specified here or in Arguments
         string filePath = args["wordsBlackList"].ToString();
         using (StreamWriter outputFile = new StreamWriter(filePath, true))
         {
+            // Check if File is Empty if no
             if (new FileInfo(filePath).Length > 0)
             {
+                // make new Line
                 outputFile.Write("\n");
             }
 
+            // Write new BadWord in File
             outputFile.Write(rawInput);
         }
 
+        // Send confirmation message
+        CPH.SendMessage(user + " added " + rawInput + " to the BadWordList", false);
         return true;
     }
 }
