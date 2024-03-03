fc532bbd796b7a28332322e69573f3ec32b90f56
Joshua Schulz-Andres <126828137+MrJSA@users.noreply.github.com>
2024-03-04 00:29:50 +0100

    Update
    Set Path to Bad Word File as Global Variable for ease of use

---
+++ RawCodeAndOldScripts/AddBadWord-2.1.0.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+
+public class CPHInline
+{
+    public bool Execute()
+    {
+        // Get raw input of the Bad Word
+        string rawInput = args["rawInput"].ToString();
+        // Get User Name
+        string user = args["userName"].ToString();
+        // Get .txt file Path specified here or in Arguments
+        string filePath = args["global_BadWordListPath"].ToString();
+        using (StreamWriter outputFile = new StreamWriter(filePath, true))
+        {
+            // Write new BadWord in File
+            outputFile.WriteLine(rawInput);
+        }
+
+        // Send confirmation message
+        CPH.SendMessage(user + " added " + rawInput + " to the BadWordList", false);
+        return true;
+    }
+}
